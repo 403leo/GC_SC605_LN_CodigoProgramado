d33a7500029b8d47d1c653df1774c1ee6085f7be
403leo <leonardomadsanchez@gmail.com>
2024-11-23 22:38:36 -0600

    Vidas Funcionales
    Ya funcionan  las vidas

--- Assets/Scripts/MenuInicial.cs
+++ Assets/Scripts/MenuInicial.cs
@@ -8,7 +8,7 @@ public class MenuInicial : MonoBehaviour
     // Start is called before the first frame update
     public void Start()
     {
-        //SceneManagement.LoadScene("SC Demo Scene - Village Props");
+        //SceneManagement.LoadScene(SceneManagement.GetActiveScene() + 1);
     }
 
     // Update is called once per frame
