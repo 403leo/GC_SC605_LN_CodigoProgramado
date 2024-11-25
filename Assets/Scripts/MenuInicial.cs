c93729af63ee21df57bba5be5d7068873d694387
JoshuaMeji <156728872+JoshuaMeji@users.noreply.github.com>
2024-11-24 18:25:51 -0600

    Menu Inicial Completo

--- Assets/Scripts/MenuInicial.cs
+++ Assets/Scripts/MenuInicial.cs
@@ -6,13 +6,13 @@ using UnityEngine.SceneManagement;
 public class MenuInicial : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void Start()
+    public void BotonStart()
     {
-        //SceneManagement.LoadScene(SceneManagement.GetActiveScene() + 1);
+        SceneManager.LoadSceneAsync(1);
     }
 
     // Update is called once per frame
-    public void Salir()
+    public void BotonSalir()
     {
         Debug.Log("Salir....");
         Application.Quit();
